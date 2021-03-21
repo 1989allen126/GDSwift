0d959a0587f07c088f3a5c779131682975f425b2
Allen <jjl13142008@126.com>
2021-03-21 20:41:07 +0800

    Bump version to 0.0.1

--- GDSwift/Classes/Core/Constant/GDConstantAndMacro.swift
+++ GDSwift/Classes/Core/Constant/GDConstantAndMacro.swift
@@ -8,29 +8,29 @@
 
 import Foundation
 
-let IPhoneX = GD.IPhoneX
+public let IPhoneX = GD.IPhoneX
 
 //屏幕相关
-let ScreenWidth = GD.ScreenWidth
-let ScreenHeight = GD.ScreenHeight
+public let ScreenWidth = GD.ScreenWidth
+public let ScreenHeight = GD.ScreenHeight
 
-let NaviBarHeight = GD.NavigationBarHeight
+public let NaviBarHeight = GD.NavigationBarHeight
 
 //字体大小
-let font8 = GDFont(8.0)
-let font10 = GDFont(10.0)
-let font12 = GDFont(12.0)
-let font13 = GDFont(13.0)
-let font14 = GDFont(13.0)
-let font16 = GDFont(16.0)
-let font18 = GDFont(18.0)
-let font20 = GDFont(20.0)
+public let font8 = GDFont(8.0)
+public let font10 = GDFont(10.0)
+public let font12 = GDFont(12.0)
+public let font13 = GDFont(13.0)
+public let font14 = GDFont(13.0)
+public let font16 = GDFont(16.0)
+public let font18 = GDFont(18.0)
+public let font20 = GDFont(20.0)
 
-let mediumFont8 = GDMFont(8.0)
-let mediumFont10 = GDMFont(10.0)
-let mediumFont12 = GDMFont(12.0)
-let mediumFont13 = GDMFont(13.0)
-let mediumFont14 = GDMFont(13.0)
-let mediumFont16 = GDMFont(16.0)
-let mediumFont18 = GDMFont(18.0)
-let mediumFont20 = GDMFont(20.0)
+public let mediumFont8 = GDMFont(8.0)
+public let mediumFont10 = GDMFont(10.0)
+public let mediumFont12 = GDMFont(12.0)
+public let mediumFont13 = GDMFont(13.0)
+public let mediumFont14 = GDMFont(13.0)
+public let mediumFont16 = GDMFont(16.0)
+public let mediumFont18 = GDMFont(18.0)
+public let mediumFont20 = GDMFont(20.0)
